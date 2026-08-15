4a370ce208c4c4763601f02b08c2bd7c581012f5
nashinnov8 <rn100204@gmail.com>
2026-08-15 09:52:34 +0700

    Package CLI as dotnet tool for distribution
    - Add PackAsTool and ToolCommandName=apiforge
    - Copy templates into output directory via Content link
    - Fix template path resolution to use AppContext.BaseDirectory
    - Enable global install via dotnet tool install

--- src/ApiForge.Cli/Program.cs
+++ src/ApiForge.Cli/Program.cs
@@ -5,9 +5,7 @@ using ApiForge.Generator.Generation;
 using ApiForge.Generator.Rendering;
 using ApiForge.Generator.Templates;
 
-// Templates nằm ở gốc repo, tính đường dẫn tương đối từ thư mục chạy CLI
-var templatesRoot = Path.Combine(AppContext.BaseDirectory, "../../../../../templates");
-templatesRoot = Path.GetFullPath(templatesRoot);
+var templatesRoot = Path.Combine(AppContext.BaseDirectory, "templates");
 
 var outputRoot = Directory.GetCurrentDirectory();
 
